c02b1e7ca51b5056d5432fa5bceb5d644c216251
matheus kenzo <matheus.yamashita@edu.unifil.br>
2022-10-04 18:30:04 -0300

    commit de teste

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,12 +1,59 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+    figurinhas_trocaveis_maria = 0
+    figurinhas_trocaveis_joao = 0
+    figurinhas_trocaveis_total = 0
+    figurinhas_diferentes_maria = 0
+    figurinhas_diferentes_joao = 0
+    aux = 0
+
+    
+    for x in figurinhas_da_maria:
+        for y in figurinhas_da_maria:
+            if (x == y):
+                figurinhas_trocaveis_maria = figurinhas_trocaveis_maria + 1
+            else:
+                figurinhas_diferentes_maria = figurinhas_diferentes_maria + 1
+
+    figurinhas_trocaveis_maria = int(figurinhas_trocaveis_maria) - int(A)
+    figurinhas_trocaveis_maria = figurinhas_trocaveis_maria/2
+    figurinhas_diferentes_maria = figurinhas_diferentes_maria/2
+
+    
+    for x in figurinhas_do_joao:
+        for y in figurinhas_do_joao:
+            if (x == y):
+                figurinhas_trocaveis_joao = figurinhas_trocaveis_joao + 1
+            else:
+                figurinhas_diferentes_joao = figurinhas_diferentes_joao + 1
+
+    figurinhas_trocaveis_joao = int(figurinhas_trocaveis_joao) - int(B)
+    figurinhas_trocaveis_joao = figurinhas_trocaveis_joao/2          
+    figurinhas_diferentes_joao = figurinhas_diferentes_joao/2
+
+    
+    if (len(figurinhas_da_maria)> len(figurinhas_do_joao)):
+        if (figurinhas_diferentes_maria == A):
+            figurinhas_trocaveis_maria = A
+
+    elif (len(figurinhas_da_maria)< len(figurinhas_do_joao)):
+        if(figurinhas_diferentes_joao == B):
+            figurinhas_trocaveis_joao = B
+
+    
+            
+        
+
+    figurinhas_trocaveis_total = figurinhas_trocaveis_maria+figurinhas_trocaveis_joao
+    print(int(figurinhas_trocaveis_total))
+    return figurinhas_trocaveis_total
 
 
 
 if __name__ == '__main__':
-    A, B = input().split(' ')
-    figurinhas_da_maria = input().split(' ')
-    figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+    A, B = input("Digite o numero total de figurinhas que Maria e Joao possuem (os numeros devem ser separados por um espaco em branco)\n").split(' ')
+
+    figurinhas_da_maria = input("Quais sao as figurinhas de Maria?(separados por um espaco em branco)\n ").split(' ')
+    
+    figurinhas_do_joao = input("Quais sao as figurinhas e Joao?(separados por um espaco em branco)\n").split(' ')
+    
+    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao)
