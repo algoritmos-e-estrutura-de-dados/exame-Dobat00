11585ef6fcfd507112cabb8b99a2c3534045e42c
matheus kenzo <matheus.yamashita@edu.unifil.br>
2022-10-04 18:41:35 -0300

    tentativa 3

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,32 +1,36 @@
-def maximizar_troca_de_figurinhas(A,B,figurinhas_da_maria, figurinhas_do_joao):
+def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
     figurinhas_trocaveis_maria = 0
     figurinhas_trocaveis_joao = 0
     figurinhas_trocaveis_total = 0
     figurinhas_diferentes_maria = 0
     figurinhas_diferentes_joao = 0
+    quantidade_figurinhas_maria =0
+    quantidade_figurinhas_joao = 0
     aux = 0
 
     
     for x in figurinhas_da_maria:
+        quantidade_figurinhas_maria = quantidade_figurinhas_maria + 1
         for y in figurinhas_da_maria:
             if (x == y):
                 figurinhas_trocaveis_maria = figurinhas_trocaveis_maria + 1
             else:
                 figurinhas_diferentes_maria = figurinhas_diferentes_maria + 1
 
-    figurinhas_trocaveis_maria = int(figurinhas_trocaveis_maria) - int(A)
+    figurinhas_trocaveis_maria = int(figurinhas_trocaveis_maria) - int(quantidade_figurinhas_maria)
     figurinhas_trocaveis_maria = figurinhas_trocaveis_maria/2
     figurinhas_diferentes_maria = figurinhas_diferentes_maria/2
 
     
     for x in figurinhas_do_joao:
+        quantidade_figurinhas_joao = quantidade_figurinhas_joao + 1
         for y in figurinhas_do_joao:
             if (x == y):
                 figurinhas_trocaveis_joao = figurinhas_trocaveis_joao + 1
             else:
                 figurinhas_diferentes_joao = figurinhas_diferentes_joao + 1
 
-    figurinhas_trocaveis_joao = int(figurinhas_trocaveis_joao) - int(B)
+    figurinhas_trocaveis_joao = int(figurinhas_trocaveis_joao) - int(quantidade_figurinhas_joao)
     figurinhas_trocaveis_joao = figurinhas_trocaveis_joao/2          
     figurinhas_diferentes_joao = figurinhas_diferentes_joao/2
 
@@ -56,4 +60,4 @@ if __name__ == '__main__':
     
     figurinhas_do_joao = input("Quais sao as figurinhas e Joao?(separados por um espaco em branco)\n").split(' ')
     
-    maximizar_troca_de_figurinhas(A,B,figurinhas_da_maria, figurinhas_do_joao)
+    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao)
