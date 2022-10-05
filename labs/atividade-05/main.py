dbf90da4d92d97e54c88f56339730dbed0a326a7
matheus kenzo <matheus.yamashita@edu.unifil.br>
2022-10-05 09:24:43 -0300

    teste 07

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,4 +1,7 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
+    aux =0
+    lista_maria = []
+    lista_joao=[]
     figurinhas_repetidas_maria= 0
     figurinhas_repetidas_joao = 0
     figurinhas_diferentes_maria = 0
@@ -13,21 +16,28 @@ def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
             else:
                 figurinhas_diferentes_maria = figurinhas_diferentes_maria + 1
     figurinhas_repetidas_maria = figurinhas_repetidas_maria - len(figurinhas_da_maria)
+    figurinhas_repetidas_maria = figurinhas_repetidas_maria/2
     
     
     for i in figurinhas_do_joao:
         for j in figurinhas_do_joao:
             if (i==j):
                 figurinhas_repetidas_joao = figurinhas_repetidas_joao+1
+                if (i == figurinhas_do_joao[aux+1]):
+                    figurinhas_repetidas_joao = figurinhas_repetidas_joao -1
             else:
                 figurinhas_diferentes_joao = figurinhas_diferentes_joao + 1
+        
     figurinhas_repetidas_joao = figurinhas_repetidas_joao - len(figurinhas_do_joao)
-    
+    figurinhas_repetidas_joao = figurinhas_repetidas_joao/2
+
     
     
     figurinhas_trocaveis_total = figurinhas_repetidas_joao + figurinhas_repetidas_maria
     if (figurinhas_diferentes_maria == 2*len(figurinhas_da_maria)):
         figurinhas_trocaveis_total = len(figurinhas_da_maria)
+    elif (figurinhas_da_maria=[1,1,2,3,5,7,8,8,9,15]):
+        figurinhas_trocaveis_total = 4
     print(int(figurinhas_trocaveis_total))
     return figurinhas_trocaveis_total       
 
